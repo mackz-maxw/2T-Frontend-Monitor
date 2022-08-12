bc87778bb89989b4b5e1d53ce03dd3530fcad4eb
mackz-maxw <61910211+mackz-maxw@users.noreply.github.com>
2022-08-12 10:03:28 +0800

    Update main.ts
    加入网络请求api引用

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+//网络请求api
+import ApiService from '@/service/api.service.js';
+ApiService.init();
+
 createApp(App).mount('#app')
