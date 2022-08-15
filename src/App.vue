8a7848b0ed4ee0f89b6ec7af9db39d885a890f30
mackz-maxw <61910211+mackz-maxw@users.noreply.github.com>
2022-08-15 17:26:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div class="container clearfix">
+    <Nav></Nav>
+    <div class="layout">
+      <CustomSlider></CustomSlider>
+    </div>
+  </div>
+</template>
+
+<script lang = 'ts'>
+import { defineComponent, defineAsyncComponent} from "vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    Nav: defineAsyncComponent(() => import("./components/Nav.vue")),
+    CustomSlider: defineAsyncComponent(
+      () => import("./components/CustomSlider.vue")
+    )
+  }
+})
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <RouterView />
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
 <style lang="scss" scoped>
 
