9eb28f22efc9dfd58828cfcd8f58ead7d6ac89dc
wangwenjun <wangwenjun@segasoft.cn>
2022-08-15 10:06:54 +0800

    [monitor-main]增加vite‘@’路径

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import App from './App.vue'
 import router from './router/index'
 
 //网络请求api
-import ApiService from '@/service/api.service.js';
-ApiService.init();
+// import ApiService from '@/service/api.service.js';
+// ApiService.init();
 
 import {useMonitor} from '../utils/monitor/index.js'
 useMonitor();
