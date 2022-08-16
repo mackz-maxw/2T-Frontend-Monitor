9bd94f16d0ea13bf141dfcefa93dae8564fcfd0c
mackz-maxw <61910211+mackz-maxw@users.noreply.github.com>
2022-08-16 14:18:01 +0800

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,60 +1,117 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/pages/HomeView/HomeView.vue';
-import HealthState from '../views/pages/healthState/healthState.vue';
-import PerformanceView from '../views/pages/performanceView/performanceView.vue';
+import HomeView from '../views/HomeView.vue';
+import userList from '../views/pages/userList.vue';
+import PerformanceView from '../views/pages/performanceView.vue';
 import JsView from '../views/test/js.vue';
 import NetworkView from '../views/test/network.vue';
-import DangerView from '../views/pages/dangerView/dangerView.vue';
+import DangerView from '../views/pages/dangerView.vue';
 
 const routes = [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      // 如果是根目录的话重定向到overview
-      redirect:"/overview",
-      children:[
-        {
-          path:"/overview",
-          name:"overview",
-          component:() => import("../views/pages/overView/overView.vue")
-        },
-        {
-          path:"/healthstate",
-          name:"healthstate",
-          component: HealthState
-        },     
-        {
-            path:"/performance",
-            name:"performance",
-            component: PerformanceView
-        },      
-        {
-            path:"/danger",
-            name:"danger",
-            component: DangerView
-        }
-      ]
-    },
-    {
-        path: '/test/js',
-        name: 'JsView',
-        component: JsView
-    }, 
-    {
-        path: '/test/network',
-        name: 'NetworkView',
-        component: NetworkView
-    }, 
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    // 如果是根目录的话重定向到overview
+    redirect:"/overview",
+    children: [
+      {
+        path: "/overview",
+        name: "overview",
+        component: () => import("../views/pages/overView.vue")
+      },
+      {
+        path: "/userlist",
+        name: "userlist",
+        component: userList
+      },
+      {
+        path: "/performance",
+        name: "performance",
+        component: PerformanceView
+      },
+      {
+        path: "/danger",
+        name: "danger",
+        component: DangerView
+      }
+    ]
+  },
+  // {
+  //   path: '/test/js',
+  //   name: 'JsView',
+  //   component: JsView
+  // },
+  // {
+  //   path: '/test/network',
+  //   name: 'NetworkView',
+  //   component: NetworkView
+  // },
+  {
+    path: '/errorStat',
+    name: 'errorStat',
+    isHide: true, 
+    component: () => import("../views/pages/errorStat.vue"),
+    redirect: "/errorStat_total",
+    children: [
+      {
+        path: "/errorStat_total",
+        name: "errorStat_total",
+        component: () => import("../views/pages/errorStat_total.vue"),
+        // redirect: "/error_table",
+        children: [
+          {
+            path: "/error_table",
+            name: "error_table",
+            component: () => import("../views/pages/error_table.vue")
+          }
+        ]
+      },
+      {
+        path: "/errorStat_list",
+        name: "errorStat_list",
+        component: () => import("../views/pages/errorStat_list.vue"),
+        children: [
+          {
+            path: "/error_table",
+            name: "error_table",
+            component: () => import("../views/pages/error_table.vue")
+          }
+        ]
+      },
+      {
+        path: "/errorStat_rele",
+        name: "errorStat_rele",
+        component: () => import("../views/pages/errorStat_rele.vue"),
+        children: [
+          {
+            path: "/error_table",
+            name: "error_table",
+            component: () => import("../views/pages/error_table.vue")
+          }
+        ]
+      },
+      {
+        path: "/errorStat_proc",
+        name: "errorStat_proc",
+        component: () => import("../views/pages/errorStat_proc.vue"),
+        children: [
+          {
+            path: "/error_table",
+            name: "error_table",
+            component: () => import("../views/pages/error_table.vue")
+          }
+        ]
+      },
+    ]
+  },
+]
 
-  ]
-  
-  const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-  })
-  
-  export default router
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
 
 // const router = createRouter({
 //     history: createWebHistory(
