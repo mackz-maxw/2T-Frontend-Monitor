0a5736121ab003066dad4f80eab1aea9941df0ec
mackz-maxw <61910211+mackz-maxw@users.noreply.github.com>
2022-08-16 14:15:45 +0800

    add errorstatus links

---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">监控系统</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">    </ul>
+
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#">首页</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">概览</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">错误</a>
+                            <ul class="dropdown-menu">
+                            <li>
+                                <router-link class="dropdown-item" :to="{name: 'errorStat'}">错误统计</router-link>
+                            </li>
+                            <li><a class="dropdown-item" href="#">Api接口错误统计</a></li>
+                            <li><a class="dropdown-item" href="#">静态资源错误统计</a></li>
+                            </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#">性能</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">实验室</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">设置</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+}
+</script>
+
+<style scoped>
+
+</style>
